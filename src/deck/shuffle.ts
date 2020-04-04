257aae4da58c9de306a46a9bfea1422b58362285
Kav Singh <flakisms@gmail.com>
2020-04-04 03:09:43 +0200

    allow mutating shuffle function, shift immut to createDeckShuffler

--- src/deck/shuffle.ts
+++ src/deck/shuffle.ts
@@ -2,40 +2,39 @@ import { Cards } from '../card/types'; // import type
 
 export type RandomIntGenerator = (min: number, max: number) => Promise<number>;
 
-export type ShuffleFunction = <T>(arr: readonly T[]) => Promise<T[]>;
+export type ShuffleFunction = <T>(arr: T[]) => Promise<T[]>;
 
 export type ShuffleFunctionCreator = (
   randomIntGenerator: RandomIntGenerator,
 ) => ShuffleFunction;
 
-// Provide a naive Math.random solution, BYO robust crypto solution
+// Provide a naive Math.random based generator, BYO robust solution
 // e.g. random-number-csprng
 export const randomIntNaive: RandomIntGenerator = (min: number, max: number) =>
   Promise.resolve(min + Math.floor(Math.random() * max));
 
-// Based on fisher-yates using async crypto secure pseudo random numbers
+// Based on fisher-yates (https://bost.ocks.org/mike/shuffle/)
 // Adapted from https://medium.com/swlh/the-javascript-shuffle-62660df19a5d
-// For info about fisher-yates: https://bost.ocks.org/mike/shuffle/
 export const createFisherYatesStackShuffle: ShuffleFunctionCreator = (
   randomIntGenerator,
 ) => async (arr) => {
-  const shuffled = [...arr];
-  let count = shuffled.length;
+  let count = arr.length;
 
   while (count) {
     const sampleIndex = await randomIntGenerator(0, count);
 
-    shuffled.push(shuffled.splice(sampleIndex, 1)[0]);
+    arr.push(arr.splice(sampleIndex, 1)[0]);
     count -= 1;
   }
 
-  return shuffled;
+  return arr;
 };
+
+export type DeckShuffler = (deck: Cards) => Promise<Cards>;
 
 export const createDeckShuffler = (shuffleFn?: ShuffleFunction) => {
   const shuffle = shuffleFn || createFisherYatesStackShuffle(randomIntNaive);
+  const shuffler: DeckShuffler = (deck) => shuffle([...deck]);
 
-  return (deck: Cards): Promise<Cards> => shuffle(deck);
+  return shuffler;
 };
-
-export type DeckShuffler = ReturnType<typeof createDeckShuffler>;
