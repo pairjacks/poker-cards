8462da44b63cc8885a6e6f5074d084517740d810
Kav Singh <flakisms@gmail.com>
2022-04-10 21:18:51 +0200

    update deps, fix mutation tests

--- stryker.conf.js
+++ stryker.conf.js
@@ -6,6 +6,7 @@ module.exports = {
     '!src/**/constants.ts',
     '!src/**/__fixtures__/*',
   ],
+  plugins: ['@stryker-mutator/jest-runner'], 
   testRunner: 'jest',
   reporters: ['progress', 'clear-text', 'html'],
   coverageAnalysis: 'all',
